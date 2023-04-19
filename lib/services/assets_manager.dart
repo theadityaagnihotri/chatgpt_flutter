3d45d0b3789ebb4da051b8e93fe6eda9e1809461
theadityaagnihotri <aditya.2125cse1127@kiet.edu>
2023-04-19 22:29:13 +0530

    update

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,5 @@
+class AssetsManager {
+  static String openailogo = "assets/images/openai_logo.png";
+  static String userImage = "assets/images/person.png";
+  static String botImage = "assets/images/chat_logo.png";
+}
